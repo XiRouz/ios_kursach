64fcef657c1e17300e1f3ccbf4208190d307adc9
semyon <semyon@semyons-Mac.local>
2024-06-09 04:51:34 -0700

    init

---
+++ kursach/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kursach
+//
+//  Created by semyon on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, stupid laggy world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kursach/kursachApp.swift
@@ -0,0 +1,17 @@
+//
+//  kursachApp.swift
+//  kursach
+//
+//  Created by semyon on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct kursachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
